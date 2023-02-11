9985dbb7b7a3b2f8be35b53220a127035dcdebde
Bailey <sabour047@gmail.com>
2023-02-11 00:45:50 -0600

    installed ejs and made placeholder project pages

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const path = require('path');
+const _ = require("lodash");
 
 const app = express();
 
-app.use(express.static("public"));
+// Set views of pages to views
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + "/public"));
+
+
+
+
+
+
+app.get("/projects/:customProjectName", function(req, res){
+
+    const customProjectName = _.capitalize(req.params.customProjectName);
+
+    res.render("project", {name: customProjectName})
+});
+
+
+
+
 
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "/index.html"));
@@ -11,11 +31,15 @@ app.get("/", function(req, res){
 
 
 
+
+
+
+
+
 let port = process.env.PORT;
 if(port == null || port == ""){
     port = 3000;
-}
-//comment 
+} 
 
 app.listen(port, () => {
     console.log("Server started on successfully")
